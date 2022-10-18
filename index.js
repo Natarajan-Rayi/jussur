7f7e76a69b19f4cabb43cdbfcbb2e85cc15a59ba
Natarajan G <mza@itvalues.dk>
2022-10-18 13:20:45 +0530

    latest commit

--- index.js
+++ index.js
@@ -166,9 +166,9 @@ app.post("/view-summary", function(req, res) {
 
 });
 
-var server = app.listen(8081, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(8081, function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port);
-});
+//   console.log("Example app listening at http://%s:%s", host, port);
+// });
