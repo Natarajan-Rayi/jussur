077d671f3b55a2a5c5b741342f36f4d58fb37071
Natarajan G <natarajan.rayi@gmail.com>
2022-11-16 15:50:59 +0530

    decrypt-code-api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const path = require('path');
 const fs = require('fs');
 const { error } = require("console");
+const payment_token = require("apple-pay-token-decrypt")
 
 var contract_id = ''
 
@@ -12,17 +13,23 @@ const indexPath = path.resolve(__dirname, "summaryPage", "summary.html");
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+const publicCert = fs.readFileSync(path.join(__dirname, './summaryPage/apple_pay.pem'), 'utf8') // import your certificate file
+const privateKey = fs.readFileSync(path.join(__dirname, './summaryPage/YourPrivateKey.pem'), 'utf8') // import your private key file
+
 const PORT = process.env.PORT || 5000;
 app.get("/", function(req, res) {
   res.send('home page')
 })
 
-app.post("/payment-success", function(req, res) {
+app.post("/payment-complete/*", function(req, res) {
   let obj_ = req.params
+  let merchant_refer = Object.values(obj_)[0]
+  console.log('merchant', merchant_refer)
+  console.log(req.body)
   if (req.body !== "" || req.body !== undefined) {
        let  data = {
             "service_command": req.body.command,
-            "merchant_reference": '6311E2A4-C343-ED11-9526-D540254B5C86',
+            "merchant_reference": merchant_refer,
             "fort_id":"",
             "status": req.body.status,
             "payment_option": req.body.payment_option,
@@ -50,51 +57,55 @@ app.post("/payment-success", function(req, res) {
             "settlement_reference": "",//27
             "phone_number": "9750593005"//28
     }
-
-    axios({
-      method: "post",
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjM2NTY1NTUsImlzcyI6Imh0dHA6Ly9KdXNzdXIuY29tIiwiYXVkIjoiaHR0cDovL0p1c3N1ci5jb20ifQ.eR9bgd088VDfTEPsDTbmcCipsMrLDV1F-IJ9uRYH7aI",
-      },
-      url:
-        "https://crm.jussuremdad.com/DEVMobAppAPI/api/payment/CompletePayment",
-      data: data,
-      // {
-      //         "service_command": req.body.command,
-      //         "merchant_reference": req.body.merchant_reference,
-      //         "fort_id": req.body.fort_id,
-      //         "status": req.body.status,
-      //         "payment_option": req.body.payment_option,
-      //         "amount": req.body.amount,
-      //         "response_code": req.body.response_code,
-      //         "card_number": req.body.card_number,
-      //         "signature": req.body.signature,
-      //         "merchant_identifier": req.body.merchant_identifier,
-      //         "access_code": req.body.access_code,
-      //         "expiry_date": req.body.expiry_date,
-      //         "customer_ip": req.body.customer_ip,
-      //         "language": req.body.language,
-      //         "eci": req.body.eci,
-      //         "command": req.body.command,//16
-      //         "response_message": req.body.response_code,//17
-      //         "authorization_code": req.body.authorization_code,//18
-      //         "customer_email": req.body.customer_email,//19
-      //         "token_name": req.body.token_name,//20
-      //         "currency": req.body.currency,//21
-      //         "card_bin": "123",//22
-      //         "card_holder_name": req.body.card_holder_name,//23
-      //         "remember_me": "",//24
-      //         "return_url": "http://localhost:3000/",//25
-      //         "customer_name": "Rayi test",//26
-      //         "settlement_reference": "",//27
-      //         "phone_number": "9750593005"//28
-      // }
-    }).then(function(response) {
-      if (response.status === 200) {
-        res.sendFile('./index.html', {root: __dirname })
+    try {
+      axios({
+        method: "post",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjM2NTY1NTUsImlzcyI6Imh0dHA6Ly9KdXNzdXIuY29tIiwiYXVkIjoiaHR0cDovL0p1c3N1ci5jb20ifQ.eR9bgd088VDfTEPsDTbmcCipsMrLDV1F-IJ9uRYH7aI",
+        },
+        url:
+          "https://crm.jussuremdad.com/DEVMobAppAPI/api/payment/CompletePayment",
+        data: data,
+      }).catch((err)=>{
+        axios({
+          method: "post",
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjM2NTY1NTUsImlzcyI6Imh0dHA6Ly9KdXNzdXIuY29tIiwiYXVkIjoiaHR0cDovL0p1c3N1ci5jb20ifQ.eR9bgd088VDfTEPsDTbmcCipsMrLDV1F-IJ9uRYH7aI",
+          },
+          url:
+            "https://crm.jussuremdad.com/DEVMobAppAPI/api/payment/CompletePayment",
+          data: data,
+         
+        }).then(function(response) {
+          if (response.status === 200) {
+          console.log(response.data)
+            res.sendFile('./index.html', {root: __dirname })
+          }
+        })
       }
-    });
+      )
+          // res.sendFile('./index.html', {root: __dirname })
+    } catch (error) {
+      axios({
+        method: "post",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjM2NTY1NTUsImlzcyI6Imh0dHA6Ly9KdXNzdXIuY29tIiwiYXVkIjoiaHR0cDovL0p1c3N1ci5jb20ifQ.eR9bgd088VDfTEPsDTbmcCipsMrLDV1F-IJ9uRYH7aI",
+        },
+        url:
+          "https://crm.jussuremdad.com/DEVMobAppAPI/api/payment/CompletePayment",
+        data: data,
+       
+      }).then(function(response) {
+        if (response.status === 200) {
+        console.log(response.data)
+          res.sendFile('./index.html', {root: __dirname })
+        }
+      });
+    }
+ 
   }
 });
 
@@ -278,6 +289,24 @@ app.get("/view-summary/*", function(req, res) {
 
 
 });
+
+app.post("/token-decrypt", function(req, res){
+  let token_get = req.body
+  // console.log(obj_)
+//   let token_get=Object.values(obj_)[0]
+//   console.log(token)
+
+  const token = new payment_token(token_get)
+const decryptedToken = token.decrypt(publicCert, privateKey)
+decryptedToken.then( ret => {
+    console.log(ret)
+    res.send(ret)
+    // return ret
+}).catch( err => {
+    console.error(err)
+    return err
+})
+})
 
 var server = app.listen(PORT, function() {
   var host = server.address().address;
